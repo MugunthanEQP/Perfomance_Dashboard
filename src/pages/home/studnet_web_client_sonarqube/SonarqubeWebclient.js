2fa64d8487d88ac1d06a0391e777899707091dbb
Mugunthan Latchoumanane <mugunthan.latchoumanane@blackboard.com>
2024-05-07 12:36:33 +0530

    changes recently done

--- src/pages/home/studnet_web_client_sonarqube/SonarqubeWebclient.js
+++ src/pages/home/studnet_web_client_sonarqube/SonarqubeWebclient.js
@@ -12,21 +12,47 @@ import {
 // Create a functional component for the web page
 
 const SonarqubeWebclient = () => {
+  const [loading, setLoading] = useState(false);
   const [selectedTestPlan, setSelectedTestPlan] = useState("");
+  const [userData, setUserData] = useState([]);
+
+  var dropdown_charts = [];
+
+  function fetchData() {
+    const url =
+      "https://nkat0vxlya.execute-api.us-east-1.amazonaws.com/proxy/testproxy/products/operations_graph";
+
+    fetch(url)
+      .then(async (response) => {
+        const jsonData = await response.json();
+        jsonData.content.measures.forEach((item) => {
+          // console.log(item.history, "check1");
+          dropdown_charts.push(
+            item.metric
+
+            //   {
+            //   label: item.metric,
+            //   value: item.history,
+            // }
+          );
+        });
+        setUserData(jsonData);
+        console.log(jsonData.content.measures.length);
+        setLoading(true);
+        console.log(dropdown_charts, "check");
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
 
   useEffect(() => {
-    fetch(
-      "http://0.0.0.0:8080/https://operations.sonar.bb-fnds.com/api/metrics/search",
-      { requestOptions, method: "GET" }
-    )
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
+    return fetchData();
   }, []);
 
-  const handleChange = (event) => {
-    setSelectedTestPlan(event.target.value);
-  };
+  // dropdown_charts.map((item) => {
+  //   console.log(item, "check3");
+  // });
 
   return (
     <Grid container spacing={2}>
@@ -34,26 +60,37 @@ const SonarqubeWebclient = () => {
         {/* Add your chart component here */}
       </Grid>
       <Grid item xs={12} sm={6} md={2}>
-        <FormControl variant="outlined" fullWidth>
-          <InputLabel id="demo-simple-select-outlined-label">
-            Dropdown
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            label="Dropdown"
-            value={handleChange}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            {/* Add your dropdown options here */}
-          </Select>
-        </FormControl>
+        {loading ? (
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Dropdown
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              label="Dropdown"
+              value={dropdown_charts}
+              
+              // onChange={handleChange}
+            >
+              {dropdown_charts.map((item, index) => (
+                <MenuItem key={index} value={item}>
+                  {item}
+                </MenuItem>
+              ))}
+              {dropdown_charts.map((item, index) => (
+                <MenuItem key={index} value={Object.values(item[0].label)}>
+                  {Object.values(item[0].label)}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        ) : (
+          ""
+        )}
       </Grid>
     </Grid>
   );
 };
-
 // Export the component
 export default SonarqubeWebclient;
